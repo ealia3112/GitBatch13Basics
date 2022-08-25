079d42fb3f7d9127d7201c7df8acc11d5814df3d
Elisa <ealia3112@gmail.com>
2022-08-24 20:04:03 -0400

    temperature check basic code is there in the repo

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+
+        //write down a code tghat checks the temp from a websitee than print out that is it a beautiful day or not
+        System.out.println("today is a beautiful day ");
+    }
+
+}
