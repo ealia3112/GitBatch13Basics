fbf32d667748ba50f0a96940f274f88232902553
Elisa <ealia3112@gmail.com>
2022-08-24 20:59:36 -0400

    temperature check basoc code 1

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -4,7 +4,7 @@ public class gitBasics {
     public static void main(String[] args) {
 
         //write down a code tghat checks the temp from a websitee than print out that is it a beautiful day or not
-        System.out.println("today is a beautiful day ");
+        System.out.println("today is not a beautiful day ");
     }
 
 }
